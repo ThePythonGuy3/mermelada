132402268858f63ab97ad7a360d6fd593c1d060b
Roger Rovira <rogerrovi2006@gmail.com>
2025-01-26 00:57:04 +0100

    Our firts enemy OH YESS

--- Assets/Scripts/Enemies/Cientific/Scientific_1.cs
+++ Assets/Scripts/Enemies/Cientific/Scientific_1.cs
@@ -1,8 +1,17 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Scientific_1 : EnemyController
 {
     [SerializeField] private ScientificBullet _scientificBullet;
+    [SerializeField] private Animator _scientistAnimatior;
+
+    private EnemyMover _enemyMover;
+
+    private float _attackTimer;
+
+    private bool _isAnimationAttackRunning = false;
 
     private void Awake()
     {
@@ -10,19 +19,59 @@ public class Scientific_1 : EnemyController
 
         attackList[0] = new Attack();
         attackList[0].isTrigger = false;
-        attackList[0].timerSeconds = 5;
+        attackList[0].timerSeconds = 3;
         attackList[0].Run = () =>
         {
             Shoot();
         };
+
+        _enemyMover = gameObject.GetComponent<EnemyMover>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (_enemyMover.isInRange)
         {
-            attackList[0].Run();
+            _scientistAnimatior.SetBool("isMoving", false);
+
+            if (_attackTimer > attackList[0].timerSeconds)
+            {
+                _attackTimer = 0;
+
+                _scientistAnimatior.SetBool("isShooting", true);
+                _isAnimationAttackRunning = true;
+
+                StartCoroutine(WaitUntilAnimationFinishes());
+
+                /*
+                attackList[0].Run();
+                _attackTimer = 0;
+
+                _scientistAnimatior.SetBool("isShooting", false);*/
+            }
         }
+        else
+        {
+            _scientistAnimatior.SetBool("isMoving", true);
+        }
+
+        _attackTimer += Time.deltaTime;
+
+    }
+
+    IEnumerator WaitUntilAnimationFinishes()
+    {
+        
+        yield return new WaitUntil(() => !_isAnimationAttackRunning);
+
+        attackList[0].Run();
+        _scientistAnimatior.SetBool("isShooting", false);
+    }
+
+    public void AnimationAttackHaveFinished()
+    {
+        Debug.Log("aaa");
+        _isAnimationAttackRunning = false;
     }
 
     private void Shoot()
