a95c769d78b7ce9ebf884d0622c5768f7eb1d57e
Roger Rovira <rogerrovi2006@gmail.com>
2025-01-25 18:57:06 +0100

    Solution to errors...

--- Assets/Scripts/Global/Player/PlayerHealth.cs
+++ Assets/Scripts/Global/Player/PlayerHealth.cs
@@ -66,7 +66,7 @@ public class PlayerHealth : MonoBehaviour
         }
         else
         {
-            Player.PlayerIsDead();
+            // Player.PlayerIsDead();
             _currentTimeHealth = 0;
         }
 
