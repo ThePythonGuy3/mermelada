54f1df515c0ea23f49bce5351f78d92a29a15218
Roger Rovira <rogerrovi2006@gmail.com>
2025-01-26 15:37:36 +0100

    Guard

---
+++ Assets/Scripts/Enemies/Guard/GuardBulletAcceptor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardBulletAcceptor : BulletAccepter
+{
+    [SerializeField] private GameObject _scientistSprite;
+    [SerializeField] private GameObject _scientistShadow;
+
+    private Animator _scientistAnimatior;
+
+    private void Awake()
+    {
+        _scientistAnimatior = _scientistSprite.GetComponent<Animator>();
+    }
+
+    public override void OnHit()
+    {
+        GameObject.FindFirstObjectByType<Manager>().Kill();
+
+        gameObject.GetComponent<NavMeshAgent>().enabled = false;
+        gameObject.GetComponent<Guard>().enabled = false;
+
+        _scientistShadow.SetActive(true);
+        _scientistSprite.GetComponent<SpriteMover>().enabled = true;
+
+        _scientistAnimatior.SetBool("hasDead", true);
+
+        StartCoroutine(DestroyScientific());
+    }
+
+    IEnumerator DestroyScientific()
+    {
+        yield return new WaitForSeconds(4);
+
+        Destroy(gameObject);
+    }
+}
